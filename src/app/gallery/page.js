3cd44d6e87baf66bcf8761b16b3a451f241bf002
md1300 <md.tawhid.khan1998@gmail.com>
2025-09-16 16:15:34 +0600

    image upload and  optimize in next js project

---
+++ src/app/gallery/page.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import HabibPhoto from "../../assets/WIN_20240205_10_54_55_Pro.jpg"
+
+const GalleryPage = () => {
+    return (
+        <div>
+          <h1>regular image</h1>
+          <img src="https://images.unsplash.com/photo-1757999524841-6f5efc86cbac?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="No Image" width={500} height={500}/>
+
+          <h1>next js Image3</h1>
+          <Image src="https://images.unsplash.com/photo-1757999524841-6f5efc86cbac?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="No Image" height={500} width={500} />
+          <h1> upload photo </h1>
+          <Image src={HabibPhoto} alt="No image" width={500} height={500}/>
+        </div>
+    );
+};
+
+export default GalleryPage;
