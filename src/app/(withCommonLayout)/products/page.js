5cba033a6f9fd5d3fa574cab118066ee77c9ef80
md1300 <md.tawhid.khan1998@gmail.com>
2025-09-17 06:41:26 +0600

    data fatch from DB server , install specific npm to run server or data

--- src/app/(withCommonLayout)/products/page.js
+++ src/app/(withCommonLayout)/products/page.js
@@ -1,9 +1,22 @@
 
 
-const ProductsPage = () => {
+const ProductsPage = async() => {
+    const res=await fetch("http://localhost:5000/posts") ;
+    const posts=await res.json()
+    console.log(posts)
     return (
         <div>
             <h1 className="text-4xl text-center">this is products page</h1>
+            <div >
+                {
+                    posts.map(({id,title,views})=><div key={id}
+                    className="bg-pink-200 m-7 p-7">
+                        <h1>post id : {id} </h1>
+                        <h1>post title : {title} </h1>
+                        <h1>post views : {views} </h1>
+                    </div>)
+                }
+            </div>
         </div>
     );
 };
