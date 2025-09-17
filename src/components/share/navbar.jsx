b661d96b58d74cbfa3ec99c6fb6aac6354482a3b
md1300 <md.tawhid.khan1998@gmail.com>
2025-09-17 14:34:23 +0600

    handle active link in next js

--- src/components/share/navbar.jsx
+++ src/components/share/navbar.jsx
@@ -1,14 +1,17 @@
+"use client"
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+    const pathName=usePathname()
     return (
         <div className="bg-blue-400 text-white flex justify-between text-3xl px-6 py-2 ">
              <Link href="/">home site</Link>
              <div className="flex gap-3">
-                <Link href="/about">About</Link>
-                <Link href="/counter">Counter</Link>
-                <Link href="/contact">Contact</Link>
-                <Link href="/dashboard">Dashboard</Link>
+                <Link href="/about" className={pathName=='/about'?"font-bold text-amber-200 underline":"hover:underline text-white cursor-pointer"}>About</Link>
+                <Link href="/counter" className={pathName=='/counter'?"font-bold text-amber-200 underline":"hover:underline text-white cursor-pointer"}>Counter</Link>
+                <Link href="/contact" className={pathName=='/contact'?"font-bold text-amber-200 underline":"hover:underline text-white cursor-pointer"}>Contact</Link>
+                <Link href="/dashboard" className={pathName=='/dashboard'?"font-bold text-amber-200 underline":"hover:underline text-white cursor-pointer"}>Dashboard</Link>
              </div>
         </div>
     );
